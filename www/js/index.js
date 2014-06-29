52f3a88f62225e16618921c60ff74e90eca91d8a
Joseph Frazier <joseph@onsip.com>
2014-06-29 10:47:08 -0400

    move phone JS to index.js, remove isSupported hack, use 2-space indents

--- www/js/index.js
+++ www/js/index.js
@@ -17,35 +17,84 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        document.removeEventListener("touchmove", cordova.plugins.phonertc.updateVideoPosition);
-        window.addEventListener("orientationchange", cordova.plugins.phonertc.updateVideoPosition);
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+    document.removeEventListener("touchmove", cordova.plugins.phonertc.updateVideoPosition);
+    window.addEventListener("orientationchange", cordova.plugins.phonertc.updateVideoPosition);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    var $ = document.querySelector.bind(document);
 
-        console.log('Received Event: ' + id);
-    }
+    $('#phone').addEventListener('submit', function (e) {
+      if (window.session) {
+        console.warn('only one call at a time');
+        return;
+      }
+
+      window.ua = window.ua || new SIP.UA({
+        traceSip: true,
+        mediaHandlerFactory: function defaultFactory (session, options) {
+          return new PhoneRTCMediaHandler(session, options);
+        }
+      });
+
+      window.session = window.ua.invite($('#target').value, {
+        media: {
+          constraints: {
+            audio: true,
+            video: $('#useVideo').checked
+          },
+          render: {
+            local: {
+              video: $('#localVideo')
+            },
+            remote: {
+              video: $('#remoteVideo')
+            }
+          }
+        }
+      });
+
+      window.session.on('terminated', function () {window.session = null;});
+
+      // adapted from http://stackoverflow.com/a/5150556
+      if (e.preventDefault) {
+         e.preventDefault();
+      }
+      e.returnValue = false; // for IE
+      return false;
+    }, false);
+
+    $('#hangup').addEventListener('click', function (e) {
+      if (window.session) {
+        window.session.terminate();
+        window.session = null;
+      }
+    }, false);
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  }
 };
